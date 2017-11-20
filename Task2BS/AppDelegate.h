d46436d723b70b58751d7b1d4b6a60ad46812849
ggcarmi <ggcarmi@gmail.com>
2017-11-20 13:50:31 +0200

    Initial Commit

---
+++ Task2BS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Task2BS
+//
+//  Created by Gai Carmi on 11/20/17.
+//  Copyright © 2017 Gai Carmi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Task2BS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Task2BS
+//
+//  Created by Gai Carmi on 11/20/17.
+//  Copyright © 2017 Gai Carmi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
